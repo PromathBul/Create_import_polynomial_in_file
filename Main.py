c6b7bdc0aebd3b5c45507fc4e669ac231a9b2353
Dmitry_Tikhonov <tdmv1209@gmail.com>
2022-10-21 16:24:21 +0300

    A program has been created that generates a polynomial with random degrees and coefficients. It also writes as text to a txt file.

--- Main.py
+++ Main.py
@@ -1,45 +1,22 @@
-from random import randint
+from Methods import create_list_natural_numbers as create
 from Methods import enter
+from Methods import power_equation as equation
 
-max = enter('fkjg')
+from Import_in_files import import_in_files as in_file
 
-def Create_list_natural_numbers (length, max):
-    nat_nums = []
-    for i in range (length):
-        nat_nums.append(randint(0, max))
-    return nat_nums
+from random import randint
+import os
 
-def Print_power_equation (coeffs):
-    equation = ''
-    for i in range (len(coeffs) - 1, 0, -1):
-        if coeffs[i] != 0:
-            if i == 1:
-                equation += f'{coeffs[i]}x + '
-            else:
-                equation += f'{coeffs[i]}x^{i} + '
-    if coeffs[i] == 0:
-        equation += ' = 0'
-    else:
-        equation += f'{coeffs[0]} = 0'
-    return equation
+os.system('cls')
 
-power = randint(1, 6)
+max_power = enter('Введите максимально возможную степень в уравнении: ')
+power = randint(1, max_power)
 max_coeff = 100
-coeffs = Create_list_natural_numbers(power + 1, max_coeff)
+
+coeffs = create (power + 1, max_coeff)
 print(coeffs)
-first_equation = Print_power_equation(coeffs)
-print(first_equation)
+my_equation = equation(coeffs)
+print(my_equation)
 
-first_file = 'First_power_equation.txt'
-with open(first_file, 'w') as data:
-    data.writelines(first_equation)
-
-power = randint(1, 5)
-coeffs = Create_list_natural_numbers(power + 1, max_coeff)
-print(coeffs)
-second_equation = Print_power_equation(coeffs)
-print(second_equation)
-
-second_file = 'Second_power_equation.txt'
-with open(second_file, 'w') as data:
-    data.writelines(second_equation)
+file = 'Power_equation.txt'
+in_file (file, my_equation)
--- Methods.py
+++ Methods.py
@@ -1,3 +1,25 @@
+from random import randint
+
 def enter (message):
     num = int(input(message))
     return num
+
+def create_list_natural_numbers (length, max):
+    nat_nums = []
+    for i in range (length):
+        nat_nums.append(randint(0, max))
+    return nat_nums
+
+def power_equation (coeffs):
+    equation = ''
+    for i in range (len(coeffs) - 1, 0, -1):
+        if coeffs[i] != 0:
+            if i == 1:
+                equation += f'{coeffs[i]}x + '
+            else:
+                equation += f'{coeffs[i]}x^{i} + '
+    if coeffs[i] == 0:
+        equation += ' = 0'
+    else:
+        equation += f'{coeffs[0]} = 0'
+    return equation
